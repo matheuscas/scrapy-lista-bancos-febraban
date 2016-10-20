f6e20d85585363c8a4b6890e681bd203ff98e76a
Matheus Cardoso <matheus.mcas@gmail.com>
2016-10-20 16:16:41 -0300

    second version of this crawler: cods are being retrieved

--- lista_bancos/lista_bancos/spiders/bancos.py
+++ lista_bancos/lista_bancos/spiders/bancos.py
@@ -11,16 +11,14 @@ class BancosSpider(scrapy.Spider):
 
     def parse(self, response):
         trs = response.xpath("//table[3]/tr/td/table/tr")
-        trs = trs[1:len(trs) - 1]
-        # trs_0_tds = trs[0].xpath("td")
-        # name = trs_0_tds[1].xpath("a/text()").extract_first()
-        # self.log(name)
+        trs = trs[1:len(trs) - 2]
         for tr in trs:
             tr_tds = tr.xpath("td")
             raw_name = tr_tds[1].xpath("a/text()").extract_first()
-            raw_name = raw_name.rstrip()
-            raw_name = raw_name.lstrip()
+            cod = tr_tds[0].xpath("text()").extract_first()
+            cod = cod if cod else ""
             yield {
-                "name" : raw_name
+                "name" : raw_name.rstrip().lstrip(),
+                "cod": cod
             }
 
